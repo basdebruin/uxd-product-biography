584bd3f5c14bc78d1a1f9e2f10da023876ac0190
Bas de Bruin <bruin998@gmail.com>
2021-03-11 15:43:41 +0100

    added page functionality

--- src/script.js
+++ src/script.js
@@ -1,9 +1,32 @@
 // scripts for uxd-product-biography
 
-// basic query function
-function q(s)    { return document.querySelector(s) }
-function qAll(s) { return document.querySelectorAll(s) }
+// basic query functions
+function q(s)      { return document.querySelector(s) }
+function qAll(s)   { return document.querySelectorAll(s) }
+function qId(s)    { return document.getElementById(s) }
 
-function toggleMenu() {
-    [q('#menu-button'), q('nav')].forEach(function(e){ e.classList.toggle('open') });
+// make menu (in)visible
+function toggleMenu(cond = undefined) {
+    [q('#menu-button'), q('nav')].forEach(function(e){ e.classList.toggle('open', cond) });
 }
+
+// hide all sections and make section visible
+function makeSectionVisible(section) {
+    if (typeof section == undefined || section == null) return;
+    console.log(section);
+    qAll('section').forEach(function(e){ e.style.display = 'none' });
+    section.style.display = 'block';
+}
+
+// listen for page change
+function hashHandler() {
+    const hash = window.location.hash;
+    console.log('hash: ', hash);
+
+    if (hash === "") return;
+    const section = qId(hash.substring(1));
+    makeSectionVisible(section);
+    toggleMenu(false);
+}
+window.addEventListener('hashchange', hashHandler);
+document.addEventListener('DOMContentLoaded', hashHandler);
