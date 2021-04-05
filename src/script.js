cf1f30f067d8580a61d8062b9e975e6cae7e8917
Bas de Bruin <bruin998@gmail.com>
2021-04-05 17:14:54 +0200

    added types and dynamic image loading

--- src/script.js
+++ src/script.js
@@ -5,17 +5,40 @@ function q(s)      { return document.querySelector(s) }
 function qAll(s)   { return document.querySelectorAll(s) }
 function qId(s)    { return document.getElementById(s) }
 
-// make menu (in)visible
-function toggleMenu(cond = undefined) {
-    [q('#menu-button'), q('nav')].forEach(function(e){ e.classList.toggle('open', cond) });
+/**
+ * toggle menu visibility
+ * @param {boolean} [override] - Optional boolean, set visibility based on override
+ */
+function toggleMenu(override = undefined) {
+    [q('#menu-button'), q('nav')].forEach(function(e){ 
+        e.classList.toggle('open', override) 
+    });
 }
 
-// hide all sections and make section visible
+/** 
+* hide all sections and make section visible
+* @param {HTMLElement} section - Section to be made visible
+*/
 function makeSectionVisible(section) {
     if (typeof section == undefined || section == null) return;
     console.log(section);
     qAll('section').forEach(function(e){ e.style.display = 'none' });
     section.style.display = 'block';
+    loadPageImages(section);
+}
+
+/**
+ * Load images on page
+ * @param {HTMLElement} section - Section within which to load content
+ */
+function loadPageImages(section) {
+    const elems = section.querySelectorAll('*[data-src]');
+    if (elems.length === 0) return;
+
+    elems.forEach(function(e) {
+        e.setAttribute('src', e.getAttribute('data-src'));
+        e.removeAttribute('data-src');
+    });
 }
 
 // listen for page change
